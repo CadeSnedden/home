f615b5d3bd8c64779cb05a1367f46fa03f4c3cbc
CadeSnedden <37352793+CadeSnedden@users.noreply.github.com>
2018-03-13 21:41:15 -0500

    Add files via upload

---
+++ ProcStarter.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Diagnostics;
+
+namespace ProcessExitSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+		//Edit Filename to which ever file is wanting to be ran.
+                Process firstProc = new Process();
+                firstProc.StartInfo.FileName = "*.bat";
+                firstProc.EnableRaisingEvents = true;
+
+                firstProc.Start();
+
+                firstProc.WaitForExit();
+
+                //You may want to perform different actions depending on the exit code.
+                Console.WriteLine("First process exited: " + firstProc.ExitCode);
+
+
+		//Edit the Filename to which ever file it wanting to be ran.
+                Process secProc = new Process();
+                secProc.StartInfo.FileName = "*.exe";
+                secProc.Start();                
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred!!!: " + ex.Message);
+                return;
+            }
+        }
+    }
+}
