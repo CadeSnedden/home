69d2bbdd8bc34e2711f491860472c24e707a50eb
Cade Snedden <37352793+CadeSnedden@users.noreply.github.com>
2018-05-30 16:51:42 -0500

    Fixed loop / made more practical

--- Command.cs
+++ Command.cs
@@ -1,78 +1,94 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
-namespace ConsoleApp2
+namespace CSTests
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+             * runs an infinite loop
+             * this loop prompts for input from the user
+             * the user-input is executed as a cmd command
+             * then the loop restarts
+             * the loop can be exited by pressing 'ctrl' + 'c' on keyboard
+             */
 
-            //run an infinite loop. 
-            //this loop prompts for input from the user
-            //the user-input is executed as a cmd command
-            //then the loop restarts
-            //the loop can be exited by pressing 'ctrl' + 'c' on the keyboard
-            //while (true)
             {
-                Console.Write("#>> "); //create the input prompt 
-                string executeMe = Console.ReadLine(); //obtain user input to execute the next command
-                ExecuteCommand(executeMe);
+                do
+                {
+                    Console.Write("#>> "); // creates input prompt
+                    string executeMe = Console.ReadLine(); // obtain user input to execute the next command
+                    ExecuteCommand(executeMe);
 
+                    if (executeMe == "exit")
+                    {
+                        try
+                        {
 
-                /* this loop never exits. this is not a good programming practice
+                            Process cmd = new Process();
+                            cmd.StartInfo.FileName = "cmd.exe";
+                            cmd.EnableRaisingEvents = true;
+
+                            cmd.Start();
+                            cmd.WaitForExit();
+                            break;
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Oops!: " + e.Message);
+                            return;
+                        }
+                    }
+
+                } while (true);
+
+                /*
+                 * This loop never exits. This is not good programming practice
                  * if you want to improve this, add an "if statement"
                  * if executeMe == "exit": exit the loop
                  */
-            }
-        }
 
+               // if (executeMe == "exit")
+               // {
+               //     break;
+               // }
+            }
+
+            Console.ReadLine();
+        }
         static void ExecuteCommand(string command)
         {
-            //This function accepts a string as input
-            //and then passes the command to cmd.exe /c "<user input>"
-
+            // function accepts strings as input
+            // then passes command to cmd.exe /c "<user input>"
             int exitCode;
             ProcessStartInfo processInfo;
             Process process;
 
-            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo = new ProcessStartInfo("cmd.exe", "/c" + command);
             processInfo.CreateNoWindow = true;
             processInfo.UseShellExecute = false;
 
-            // *** Redirect the output ***
+            //**Redirect Output**
             processInfo.RedirectStandardError = true;
             processInfo.RedirectStandardOutput = true;
 
             process = Process.Start(processInfo);
 
             process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
-                Console.WriteLine("output>>" + e.Data);
+                Console.WriteLine("" + e.Data);
             process.BeginOutputReadLine();
 
-            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
-                Console.WriteLine("error>>" + e.Data);
-            process.BeginErrorReadLine();
+            //process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+            //    Console.WriteLine("error>> " + e.Data);
+            //process.BeginErrorReadLine();
 
             process.WaitForExit();
 
-            // *** Read the streams ***
-            // Warning: This approach can lead to deadlocks
-            /*string output = process.StandardOutput.ReadtoEnd();
-            string error = process.StandardError.ReadtoEnd(); */
-
             exitCode = process.ExitCode;
-            Console.WriteLine("ExitCode: {0}", process.ExitCode);
-
-            //comment out for testing
-            /*Console.WriteLine("output>>" + (String.IsNullOrEmpty(output) ? "(none)" : output));
-            Console.WriteLine("error>>" + (String.IsNullOrEmpty(error) ? "(none)" : error));
-            Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
-            process.Close(); */
+           // Console.WriteLine("ExitCode: {0}", process.ExitCode);
 
         }
     }
